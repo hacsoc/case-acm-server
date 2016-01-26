c6fb488db88ad7ad77c6b0014e4b46290f8733ca
Matthew Bentley <matthew.t.bentley@gmail.com>
2016-01-25 20:48:17 -0500

    New services, update scripts

--- net_start.sh
+++ net_start.sh
@@ -10,11 +10,12 @@ sleep 1
 PID=0
 while [[ PID -eq 0 ]]; do
     PID=`docker inspect -f '{{.State.Pid}}' ${NAME}`
-    sleep 10
+    sleep 1
 done
 sleep 1
 
 ln -s /proc/${PID}/ns/net /var/run/netns/${PID}
+ls /var/run/netns
 sleep 1
 
 ip link add ${NAME} link eth0 address ${MAC} type macvlan mode bridge
@@ -24,6 +25,9 @@ sleep 1
 ip netns exec ${PID} ip route del default
 sleep 1
 ip netns exec ${PID} ip a
+sleep 1
+echo "PUTTING UP ${NAME} INTERFACE!!!!!"
+ip netns exec ${PID} ip l set dev ${NAME} up
 sleep 1
 ip netns exec ${PID} dhcpcd ${NAME}
 
--- net_stop.sh
+++ net_stop.sh
@@ -8,4 +8,8 @@ PID=`docker inspect -f '{{.State.Pid}}' ${NAME}`
 
 ip netns exec ${PID} dhcpcd -k ${NAME} || true
 
-sleep 5
+rm /var/run/netns/${PID}
+
+/usr/local/bin/docker-compose -f /srv/acm/${NAME}.yml stop
+
+/usr/bin/docker rm $(/usr/local/bin/docker-compose -f /srv/acm/${NAME}.yml ps -q)
